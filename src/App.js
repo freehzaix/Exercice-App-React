15067219cd37b82cdd31db1e2d78eac4084c65b3
Jean-Luc DOH <freehzaix@gmail.com>
2023-11-08 16:52:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import AppLink from './fonctions/AppLink.js';
+
 
 function App() {
   return (
@@ -7,16 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modifiez le fichier <code>src/App.js</code> et enregistrer votre code.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <AppLink/>
+
       </header>
     </div>
   );
---
+++ src/fonctions/AppLink.js
@@ -0,0 +1,15 @@
+
+const AppLink = () => {
+    return(
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        En savoir plus sur React
+      </a>
+    )
+  }
+
+export default AppLink
